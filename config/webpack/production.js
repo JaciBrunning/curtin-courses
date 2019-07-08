779a6116096bbf25b408845a23b750307fb215b0
Jaci <jaci.brunning@gmail.com>
2019-07-09 01:26:23 +0800

    Temporarily disable minification in prod

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+// module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+delete(config.optimization.minimizer)
+module.exports = config
